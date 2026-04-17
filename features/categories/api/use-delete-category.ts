61faf494d7b4a9130e0884fe3e59ae7ba48f9088
Bhushan Lagare <bhushanlagare7@gmail.com>
2026-04-17 20:02:27 +0530

    feat(categories): add category management features

---
+++ features/categories/api/use-delete-category.ts
@@ -0,0 +1,37 @@
+import { InferResponseType } from "hono";
+import { toast } from "sonner";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+
+import { client } from "@/lib/hono";
+
+type ResponseType = InferResponseType<
+  (typeof client.api.categories)[":id"]["$delete"]
+>;
+
+export const useDeleteCategory = (id?: string) => {
+  const queryClient = useQueryClient();
+  return useMutation<ResponseType, Error>({
+    mutationFn: async () => {
+      if (!id) {
+        throw new Error("Category ID is required for deletion");
+      }
+      const response = await client.api.categories[":id"]["$delete"]({
+        param: { id },
+      });
+      if (!response.ok) {
+        throw new Error("Failed to delete category");
+      }
+
+      return await response.json();
+    },
+    onSuccess: () => {
+      toast.success("Category deleted successfully");
+      queryClient.invalidateQueries({ queryKey: ["category", { id }] });
+      queryClient.invalidateQueries({ queryKey: ["categories"] });
+      // TODO: Invalidate summary and transactions
+    },
+    onError: () => {
+      toast.error("Failed to delete category");
+    },
+  });
+};
---
+++ features/categories/api/use-edit-category.ts
@@ -0,0 +1,41 @@
+import { InferRequestType, InferResponseType } from "hono";
+import { toast } from "sonner";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+
+import { client } from "@/lib/hono";
+
+type RequestType = InferRequestType<
+  (typeof client.api.categories)[":id"]["$patch"]
+>["json"];
+type ResponseType = InferResponseType<
+  (typeof client.api.categories)[":id"]["$patch"]
+>;
+
+export const useEditCategory = (id?: string) => {
+  const queryClient = useQueryClient();
+  return useMutation<ResponseType, Error, RequestType>({
+    mutationFn: async (json) => {
+      if (!id) {
+        throw new Error("Category ID is required");
+      }
+      const response = await client.api.categories[":id"]["$patch"]({
+        param: { id },
+        json,
+      });
+      if (!response.ok) {
+        throw new Error("Failed to edit category");
+      }
+
+      return await response.json();
+    },
+    onSuccess: () => {
+      toast.success("Category edited successfully");
+      queryClient.invalidateQueries({ queryKey: ["category", { id }] });
+      queryClient.invalidateQueries({ queryKey: ["categories"] });
+      // TODO: Invalidate summary and transactions
+    },
+    onError: () => {
+      toast.error("Failed to edit category");
+    },
+  });
+};
